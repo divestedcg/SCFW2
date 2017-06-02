2b68da9cdde64a2091d1b66f722b68b3a3be6083
Tad <tad@spotco.us>
2017-06-02 06:27:20 -0400

    Dramatically improved IPv6 support

--- PKGBUILD
+++ PKGBUILD
@@ -1,13 +1,13 @@
 # Maintainer: Tad D <tad@spotco.us>
 pkgname=scfw
-pkgver=2.1
+pkgver=2.2
 pkgrel=1
 pkgdesc="A better firewall"
 arch=('any')
 license=('custom')
 depends=('iptables')
-source=('scfw.sh' 'scfw_config.sh' 'scfw.service' 'restartscfw')
-md5sums=('SKIP' 'SKIP' '00c47c3e2191cfae9bb0da6b756375f6' 'e3b58cb6c2c4e63bc052dfae79431f9e')
+source=('scfw.sh' 'scfw_config.sh' 'scfw.service' 'restartscfw' 'iptables46')
+md5sums=('SKIP' 'SKIP' '6238a17c6646d50277a949fb6f71ac99' 'a832d60f0356dd82e1d8ff8f3ce60e2f' '9caa97491f111af13ad9635972bfdc41')
 install=scfw.install
 
 build() {
@@ -16,6 +16,7 @@ build() {
 
 package() {
   cd "$srcdir"
+  install -Dm700 iptables46 "$pkgdir/usr/bin/iptables46"
   install -Dm700 restartscfw "$pkgdir/usr/bin/restartscfw"
   install -Dm600 scfw.service "$pkgdir/usr/lib/systemd/system/scfw.service"
   install -Dm700 scfw.sh "$pkgdir/usr/lib/scfw.sh"
--- scfw.sh
+++ scfw.sh
@@ -16,19 +16,19 @@ sysctl -w net/netfilter/nf_conntrack_tcp_loose=0
 #START OF HELPER FUNCTIONS
 #
 allowTCP() {
-	iptables -A INPUT -p tcp --dport $1 -j ACCEPT;
+	iptables46 -A INPUT -p tcp --dport $1 -j ACCEPT;
 }
 export allowTCP;
 
 allowUDP() {
-	iptables -A INPUT -p udp --dport $1 -j ACCEPT;
+	iptables46 -A INPUT -p udp --dport $1 -j ACCEPT;
 }
 export allowUDP;
 
 protect() {
 	allowTCP $1;
-	iptables -t raw -I PREROUTING -p tcp -m tcp --syn -j CT --notrack --dport $1;
-	iptables -I INPUT -p tcp -m tcp -m state --state INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460 --dport $1;
+	iptables46 -t raw -I PREROUTING -p tcp -m tcp --syn -j CT --notrack --dport $1;
+	iptables46 -I INPUT -p tcp -m tcp -m state --state INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460 --dport $1;
 }
 export protect;
 #
@@ -38,37 +38,37 @@ export protect;
 
 #
 #START OF PROTECTION RULES
-#Credit: https://javapipe.com/iptables-ddos-protection
+#Credit: https://javapipe.com/iptables46-ddos-protection
 #
 #Drop routed, and allow outgoing
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
+iptables46 -P FORWARD DROP
+iptables46 -P OUTPUT ACCEPT
 
 #Drop invalid packets
-iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+iptables46 -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
 
 #Drop TCP packets that are new and are not SYN
-iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 
 #Drop SYN packets with suspicious MSS value
 iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 ip6tables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 1220:65535 -j DROP
 
 #Block packets with bogus TCP flags
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+iptables46 -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
 #Block spoofed packets
 #iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
@@ -88,11 +88,11 @@ iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
 iptables -t mangle -A PREROUTING -f -j DROP
 
 #Limit connections per source IP
-iptables -A INPUT -p tcp -m connlimit --connlimit-above 32 -j REJECT --reject-with tcp-reset
+iptables46 -A INPUT -p tcp -m connlimit --connlimit-above 32 -j REJECT --reject-with tcp-reset
 
 #Limit RST packets
-iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+iptables46 -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+iptables46 -A INPUT -p tcp --tcp-flags RST RST -j DROP
 #
 #END OF RULES
 #
@@ -111,11 +111,11 @@ source /etc/scfw_config.sh
 #FINISHING TOUCHES
 #
 #Drop incomming
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -P INPUT DROP
+iptables46 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables46 -A INPUT -i lo -j ACCEPT
+iptables46 -A OUTPUT -o lo -j ACCEPT
+iptables46 -P INPUT DROP
 #Drop SYNPROXY invalid
-iptables -A INPUT -m state --state INVALID -j DROP
+iptables46 -A INPUT -m state --state INVALID -j DROP
 #More sysctls
 sysctl -w net/netfilter/nf_conntrack_tcp_loose=0
