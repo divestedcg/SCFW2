26bf43ecdd6c056c2480db72ee394f4fe506dcdf
Tad <tad@spotco.us>
2017-06-02 20:45:17 -0400

    Add an installer script for lesser systems

---
+++ install.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#Copyright (c) 2017 Spot Communications, Inc.
+
+install -Dm700 iptables46 /usr/bin/iptables46
+install -Dm700 restartscfw /usr/bin/restartscfw
+install -Dm600 scfw.service /usr/lib/systemd/system/scfw.service
+install -Dm700 scfw.sh /usr/lib/scfw.sh
+install -Dm700 scfw_config.sh /etc/scfw_config.sh
